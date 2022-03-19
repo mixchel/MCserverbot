6f085060d11b2ba1321f30cf0e772d5b3334d87e
Michel Silva <michelccs2003@gmail.com>
2022-03-19 13:52:08 -0300

    Changed message to command

--- bot.py
+++ bot.py
@@ -1,18 +1,22 @@
 import os
 import discord 
 from dotenv import load_dotenv
+from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
-@client.event
-async def on_message(message):
-    if message.content == '/startMC':
-        os.system('~/MCserverbot/start.sh')
-        await message.channel.send('Server Started')
-    if message.content == '/stopMC':
-        os.system('~/MCserverbot/stop.sh')
-        await message.channel.send('Server Stopped')
-    if message.content == '/ipMC':
-        os.popen('grep server-ip ~/MCserver/server.properties')
-        await message.channel.send(os.popen('grep server-ip ~/MCserver/server.properties').read())
-client.run(TOKEN)
+bot = commands.Bot(command_prefix='>')
+
+@bot.command(name='ip')
+async def ipcheck(ctx):
+    await ctx.send(os.popen('grep server-ip ~/MCserver/server.properties').read())
+@bot.command(name='start')
+async def start(ctx):
+    os.system('~/MCserverbot/start.sh')
+    await ctx.send('Server Started')
+@bot.command(name='stop')
+async def stop(ctx):
+    os.system('~/MCserverbot/stop.sh')
+    await ctx.send('Server Stopped')
+bot.run(TOKEN)
+
