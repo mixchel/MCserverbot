6da544f884442c9ac06d9012b04e7fdbf7750377
Michel Castro <42982212+mixchel@users.noreply.github.com>
2022-04-14 17:45:31 -0300

    added ssh to the start of every bash command

--- bot.py
+++ bot.py
@@ -9,14 +9,14 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 bot = commands.Bot(command_prefix='>')
 def get_ip():
-    return (os.popen('grep server-ip ~/MCserver/server.properties').read())[10:]
+    return (os.popen('ssh michel@192.168.15.88 grep server-ip ~/MCserver/server.properties').read())[10:]
 server = MinecraftServer.lookup(get_ip())
 @bot.command(name='ip')
 async def ipcheck(ctx):
     await ctx.send(get_ip())
 @bot.command(name='start')
 async def start(ctx):
-    os.system('~/MCserverbot/start.sh')
+    os.system('ssh michel@192.168.15.88 ~/MCserverbot/start.sh')
     await ctx.send('Starting Server...')
     for i in range (4):
         time.sleep(30)
@@ -32,7 +32,7 @@ async def start(ctx):
     if started == 0:print("There was an error starting server")
 @bot.command(name='stop')
 async def stop(ctx):
-    os.system('~/MCserverbot/stop.sh')
+    os.system('ssh michel@192.168.15.88 ~/MCserverbot/stop.sh')
     await ctx.send('Server Stopped')
 bot.run(TOKEN)
 
