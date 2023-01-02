34a6815ca406b425834299abe21373b81f582e96
Michel Castro <michelccs2003@gmail.com>
2023-01-02 21:27:36 +0000

    added check for already on server on start command

--- bot.py
+++ bot.py
@@ -14,6 +14,13 @@ client = discord.Client(intents=my_intents)
 bot = commands.Bot(command_prefix='>', intents=my_intents)
 def get_ip():
     return "104.248.230.246"
+def server_is_on():
+    try:
+        server.ping()
+    except TimeoutError:
+        return 0
+    else:
+        return 1
 # Defining the Server
 server = JavaServer.lookup(get_ip())
 @bot.command(name='ip')
@@ -21,20 +28,19 @@ async def ipcheck(ctx):
     await ctx.send(get_ip())
 @bot.command(name='start')
 async def start(ctx):
-    os.system('~/MCserverbot/start.sh')
-    await ctx.send('Starting Server...')
-    for i in range (4):
-        time.sleep(30)
-        try:
-            server.ping()
-        except:
-            started = 0
-        else:
-            await ctx.send('Server started sucesfully')
-            started = 1
-            break
-    print(started)
-    if started == 0:print("There was an error starting server")
+    if not server_is_on():
+        os.system('~/MCserverbot/start.sh')
+        await ctx.send('Starting Server...')
+        started == 0
+        for i in range (4):
+            time.sleep(30)
+            if server_is_on():
+                await ctx.send('Server started sucesfully')
+                started = 1
+                break
+        if started == 0:print("There was an error starting server")
+    else:
+        await ctx.send("Server was already on")
 @bot.command(name='stop')
 async def stop(ctx):
     os.system('~/MCserverbot/stop.sh')
